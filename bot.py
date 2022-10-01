42ce11da163623535c35d17981ab649347d39624
Sergey Kalistratov <sergey.kalistratov@avast.com>
2022-10-01 17:07:34 +0200

    huh

--- bot.py
+++ bot.py
@@ -12,11 +12,10 @@ dispatcher = updater.dispatcher
 # set up the introductory statement for the bot when the /start command is invoked
 def start(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello there. Provide any English word and I will give you a bunch "
-                                                   "of information about it.")
-
+    context.bot.send_message(chat_id=chat_id, text="Hello world")
 
 # run the start function when the user invokes the /start command 
 dispatcher.add_handler(CommandHandler("start", start))
+
 
 updater.start_polling()
