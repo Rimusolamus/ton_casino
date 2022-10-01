372b0b41d3d0ffb342532b45736e47a22a7f90d1
Sergey Kalistratov <sergey.kalistratov@avast.com>
2022-10-01 17:31:15 +0200

    what if I mirror msg

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
-import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
-from main import get_info
 
 telegram_bot_token = "5635002243:AAGvx_tNLzicrf6m_fEER247mHXou4zy0GE"
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
-
 # set up the introductory statement for the bot when the /start command is invoked
 def start(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello world")
+    context.bot.send_message(chat_id=chat_id, text="Hello, it's time to win or lose some money, using TON")
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
 
 # run the start function when the user invokes the /start command 
 dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(Filters.text, echo)
 
 
 updater.start_polling()
